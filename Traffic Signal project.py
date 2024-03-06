9358aa042f954136d769e7c083a27b32e5d9894a
Sharvesh77 <152675158+Sharvesh77@users.noreply.github.com>
2024-03-06 18:07:37 +0530

    Add files via upload

---
+++ Traffic Signal project.py
@@ -0,0 +1,8 @@
+Light = input("Enter the Traffic Light: ")
+Light = Light.lower()
+if Light == "red":
+    print("Stop see the Traffic Light it's Red")
+elif Light == "yellow":
+    print("wait after 5 seconds Go")
+else:
+    print("Go after the yellow signal")
